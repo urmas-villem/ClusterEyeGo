cc8e88c7fe564bff65634003ae5ae6ea508b7d00
unknown <urmas.villem.sundja@gmail.com>
2024-06-18 14:53:49 +0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -11,30 +11,27 @@ import (
 )
 
 func main() {
-	// creates the in-cluster config
+	// Creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
-	// creates the clientset
+	// Creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 	for {
-		// get pods in all the namespaces by omitting namespace
-		// Or specify namespace to get pods in particular namespace
 		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
 		if err != nil {
 			panic(err.Error())
 		}
 		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		// Iterate over each pod and their containers to list all images
-		fmt.Println("Images used by the pods:")
 		for _, pod := range pods.Items {
+			fmt.Printf("Pod Name: %s\n", pod.Name)
 			for _, container := range pod.Spec.Containers {
-				fmt.Println(container.Image) // Print each container's image
+				fmt.Printf("  Container Name: %s, Image: %s\n", container.Name, container.Image)
 			}
 		}
 
