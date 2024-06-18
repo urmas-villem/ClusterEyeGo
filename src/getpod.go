46b6aaaaf6bc4ff4870aad28c6dcd995f0fa6103
unknown <urmas.villem.sundja@gmail.com>
2024-06-18 15:14:53 +0300

    Update getpod.go

--- src/getpod.go
+++ src/getpod.go
@@ -3,19 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
+var softwareFilters = []string{"argocd", "jenkins", "prometheus", "alertmanager", "clustereye", "logstash"}
+
 func GetPodInfo() {
-	// Creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
-	// Creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
@@ -28,9 +29,20 @@ func GetPodInfo() {
 	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
 	for _, pod := range pods.Items {
-		fmt.Printf("Pod Name: %s\n", pod.Name)
-		for _, container := range pod.Spec.Containers {
-			fmt.Printf("  Container Name: %s, Image: %s\n", container.Name, container.Image)
+		if containsAny(pod.Name, softwareFilters) {
+			fmt.Printf("Pod Name: %s\n", pod.Name)
+			for _, container := range pod.Spec.Containers {
+				fmt.Printf("  Container Name: %s, Image: %s\n", container.Name, container.Image)
+			}
 		}
 	}
+}
+
+func containsAny(podName string, filters []string) bool {
+	for _, filter := range filters {
+		if strings.Contains(podName, filter) {
+			return true
+		}
+	}
+	return false
 }
