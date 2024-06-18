32d81f3e6671232c05a0a07f73f0c31bc2854d00
unknown <urmas.villem.sundja@gmail.com>
2024-06-18 13:31:30 +0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// uses the current context in kubeconfig
+	// path-to-kubeconfig -- for example, /root/.kube/config
+	config, _ := clientcmd.BuildConfigFromFlags("", "<path-to-kubeconfig>")
+	// creates the clientset
+	clientset, _ := kubernetes.NewForConfig(config)
+	// access the API to list pods
+	pods, _ := clientset.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{})
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 }
