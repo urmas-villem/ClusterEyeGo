77e7a389329aa363b9bedc48e9085088c64d5ae0
unknown <urmas.villem.sundja@gmail.com>
2024-06-19 13:34:18 +0300

    asd

---
+++ src/helper.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type Software struct {
+	Name          string
+	Repositories  map[string]string
+	LatestVersion string
+}
+
+func PrintResults(softwares map[string]*Software, softwareInfo *prometheus.GaugeVec) {
+	softwareInfo.Reset()
+
+	fmt.Println("Software found on the cluster:")
+	for _, software := range softwares {
+		if len(software.Repositories) > 0 {
+			fmt.Printf("%s:\n", software.Name)
+			for repo, currentVersion := range software.Repositories {
+				fmt.Printf("  repository: %s\n", repo)
+				fmt.Printf("  current-version: %s\n", currentVersion)
+				fmt.Printf("  latest-version: %s\n", software.LatestVersion)
+
+				softwareInfo.WithLabelValues(software.Name, repo, currentVersion, software.LatestVersion).Set(1)
+			}
+		}
+	}
+
+	fmt.Println("\nSoftware not found on the cluster:")
+	for _, software := range softwares {
+		if len(software.Repositories) == 0 {
+			fmt.Printf("%s:\n", software.Name)
+		}
+	}
+}
+
+func getConfigMap(clientset *kubernetes.Clientset, name, namespace string) (map[string]map[string]string, error) {
+	cm, err := clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	configData := make(map[string]map[string]string)
+	for key, jsonStr := range cm.Data {
+		var result map[string]string
+		if err := json.Unmarshal([]byte(jsonStr), &result); err != nil {
+			return nil, fmt.Errorf("error parsing ConfigMap key %s: %v", key, err)
+		}
+		configData[key] = result
+	}
+	return configData, nil
+}
+
+func sanityCheckGithub() bool {
+	url := "https://api.github.com/repos/argoproj/argo-cd/releases"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Failed to check GitHub rate limit: %v\n", err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Failed to read response body: %v\n", err)
+		return false
+	}
+
+	var apiResponse map[string]interface{}
+	if err := json.Unmarshal(body, &apiResponse); err != nil {
+		log.Printf("Error unmarshalling GitHub response: %v\n", err)
+		return false
+	}
+
+	if message, exists := apiResponse["message"]; exists && message == "API rate limit exceeded for your IP. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)" {
+		log.Println("GitHub API hourly limit reached, try again later.")
+		return true
+	}
+
+	return false
+}
--- src/main.go
+++ src/main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
-
-type Software struct {
-	Name          string
-	Repositories  map[string]string
-	LatestVersion string
-}
 
 var (
 	reg = prometheus.NewRegistry()
@@ -37,47 +27,6 @@ func init() {
 	reg.MustRegister(softwareInfo)
 }
 
-func PrintResults(softwares map[string]*Software) {
-	softwareInfo.Reset()
-
-	fmt.Println("Software found on the cluster:")
-	for _, software := range softwares {
-		if len(software.Repositories) > 0 {
-			fmt.Printf("%s:\n", software.Name)
-			for repo, currentVersion := range software.Repositories {
-				fmt.Printf("  repository: %s\n", repo)
-				fmt.Printf("  current-version: %s\n", currentVersion)
-				fmt.Printf("  latest-version: %s\n", software.LatestVersion)
-
-				softwareInfo.WithLabelValues(software.Name, repo, currentVersion, software.LatestVersion).Set(1)
-			}
-		}
-	}
-
-	fmt.Println("\nSoftware not found on the cluster:")
-	for _, software := range softwares {
-		if len(software.Repositories) == 0 {
-			fmt.Printf("%s:\n", software.Name)
-		}
-	}
-}
-
-func getConfigMap(clientset *kubernetes.Clientset, name, namespace string) (map[string]map[string]string, error) {
-	cm, err := clientset.CoreV1().ConfigMaps(namespace).Get(context.TODO(), name, metav1.GetOptions{})
-	if err != nil {
-		return nil, err
-	}
-	configData := make(map[string]map[string]string)
-	for key, jsonStr := range cm.Data {
-		var result map[string]string
-		if err := json.Unmarshal([]byte(jsonStr), &result); err != nil {
-			return nil, fmt.Errorf("error parsing ConfigMap key %s: %v", key, err)
-		}
-		configData[key] = result
-	}
-	return configData, nil
-}
-
 func main() {
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -93,21 +42,24 @@ func main() {
 		log.Fatal(http.ListenAndServe(":9191", nil))
 	}()
 
+	if sanityCheckGithub() {
+		return
+	}
+
 	repoConfig, err := getConfigMap(clientset, "clustereye-config", "default")
 	if err != nil {
-		fmt.Println("Failed to get config map:", err)
+		log.Println("Failed to get config map:", err)
 		return
 	}
 
 	for {
 		softwares, err := GetPodInfo(repoConfig["github_search"], repoConfig["elastic_search"])
 		if err != nil {
-			fmt.Printf("Error fetching pod information: %v\n", err)
+			log.Printf("Error fetching pod information: %v\n", err)
 			continue
 		}
 
-		UpdateSoftwareVersions(softwares, repoConfig["github_search"], repoConfig["elastic_search"])
-		PrintResults(softwares)
+		PrintResults(softwares, softwareInfo)
 		time.Sleep(3600 * time.Second)
 	}
 }
